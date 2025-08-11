42daabe0315da25bfdf36d49fa42c517010e6382
joaopedrossousa <jpsousa.ti@gmail.com>
2025-08-11 13:37:52 -0300

    Estudo sobre manipulação de Arquivos

---
+++ main.py
@@ -0,0 +1,7 @@
+# Função open("Localização do Arquvivo", Modo de Abertura do Arquivo)
+# Ex de modos de abertura:
+# "r" = read (Ler); "w" = whrite (Escrever), "a" = append (Escreve no final do Arquivo), "x" (Cria um novo arquivo e escreve)
+
+arquivo = open("log.txt", "a", encoding="utf-8")
+
+arquivo.write(str(input("Escreva algo: ") + "\n"))

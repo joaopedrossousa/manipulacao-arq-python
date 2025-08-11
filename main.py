1d75eaceb16462fc0e30992b5f2bd11dbf3ac38f
joaopedrossousa <jpsousa.ti@gmail.com>
2025-08-11 13:56:31 -0300

    Teste criação de multiplos arquivos direcionados a um diretório

--- main.py
+++ main.py
@@ -2,6 +2,33 @@
 # Ex de modos de abertura:
 # "r" = read (Ler); "w" = whrite (Escrever), "a" = append (Escreve no final do Arquivo), "x" (Cria um novo arquivo e escreve)
 
+"""
 arquivo = open("log.txt", "a", encoding="utf-8")
 
 arquivo.write(str(input("Escreva algo: ") + "\n"))
+
+"""
+
+cont = 1
+
+while True:
+
+    arquivo_log = open(
+        f"C:\\Users\\João Pedro Sousa\\Desktop\\python\\manipulaçãoDeArquivos\\arquivos_log\\log_cadastro_{cont}.txt",
+        "w",
+        encoding="utf-8",
+    )
+
+    arquivo_log.write(str(input(f"[Arquivo N° {cont}] \nEscreva algo: ")))
+
+    flag_continuar = (
+        str(input("Deseja continuar? [S] para SIM, [N] para NÃO: ")).strip().upper()
+    )
+
+    cont += 1
+
+    arquivo_log.close()
+
+    if flag_continuar == "N":
+        print("Programa encerrado...")
+        break
